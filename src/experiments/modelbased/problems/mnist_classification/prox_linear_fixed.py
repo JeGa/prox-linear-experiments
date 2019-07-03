dd81ad8c296b2ccb7a6bf665fafcae451f62aba7
Jens <530288+JeGa@users.noreply.github.com>
2019-07-03 10:05:32 +0200

    Added cli.

--- src/experiments/modelbased/problems/mnist_classification/prox_linear_fixed.py
+++ src/experiments/modelbased/problems/mnist_classification/prox_linear_fixed.py
@@ -1,14 +1,11 @@
-import click
-import torch
 import logging
+import torch
 
 import modelbased.data.utils
-import modelbased.utils.trainrun
-import modelbased.utils.results
-import modelbased.utils.misc
-import modelbased.utils.yaml
-import modelbased.problems.mnist_classification.misc
 import modelbased.problems.mnist_classification.prox_linear as prox_linear
+import modelbased.utils.misc
+import modelbased.utils.results
+import modelbased.utils.trainrun
 
 logger = logging.getLogger(__name__)
 
@@ -84,26 +81,3 @@ class FixedStepsize(prox_linear.SVM_OVA_ProxLinear):
         )
 
         return results
-
-
-@click.command()
-@click.option('--train-samples', required=True, type=int, help='Set to -1 to use all training data.')
-@click.option('--num-epochs', required=True, type=int)
-@click.option('--batch-size', required=True, type=int)
-@click.option('--lam', required=True, type=int)
-@click.option('--step-size', required=True, type=int)
-def argrun(train_samples, num_epochs, batch_size, lam, step_size):
-    """
-    Train MNIST classificator using prox-linear with fixed step size.
-    """
-    if train_samples == -1:
-        train_samples = None
-
-    trainloader, testloader, train_size, test_size = modelbased.problems.mnist_classification.misc.data(train_samples,
-                                                                                                        train_samples,
-                                                                                                        batch_size,
-                                                                                                        batch_size)
-
-    classificator = FixedStepsize()
-    results = classificator.run(trainloader, data_size=train_size, num_epochs=num_epochs, lam=lam, step_size=step_size)
-    modelbased.utils.yaml.write_result(results)
--- src/experiments/modelbased/run.py
+++ src/experiments/modelbased/run.py
@@ -2,26 +2,21 @@ import matplotlib
 
 matplotlib.use('Agg')
 import logging
+import click
 
-import problems.mnist_classification.prox_linear_fixed as plfixed
-
-import problems.simple_regression.prox_linear as robust_regression_prox_linear
-import problems.simple_regression.gradient_descent as robust_regression_gradient_descent
-import problems.spirals_classification.gradient_descent as spirals_classification
-
+import modelbased.problems.mnist_classification.clirun as mnist_cls_clirun
 import modelbased.utils.misc
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(mnist_cls_clirun.fixed)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(name)s:%(funcName)s - %(message)s")
     modelbased.utils.misc.make_folders()
 
-    run = 'mnist-cls'
-
-    if run == 'mnist-cls':
-        plfixed.argrun.callback(train_samples=10, num_epochs=1, batch_size=10, lam=0, step_size=15)
-    elif run == 'spirals-cls':
-        spirals_classification.run()
-    elif run == 'exp-reg':
-        robust_regression_prox_linear.run()
-    elif run == 'exp-reg-gd':
-        robust_regression_gradient_descent.run()
+    cli()
